93c3e8233e37327469dde707c5f6efeb933543e0
francescotrudu <francescotrudu84@gmail.com>
2023-01-16 12:29:38 +0100

    Add input search text, import bootstrap

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -16,10 +16,18 @@ export default {
 <template>
   <header>
     <h1>Header</h1>
+    <label class="me-2" for="searchbar">Cerca</label>
+    <input type="text" name="" id="searchbar">
+    <button class="btn btn-danger ms-2">Cerca</button>
+    <ul>
+      <li></li>
+    </ul>
 
   </header>
 </template>
 
 <style lang="scss" scoped>
-
+@use "../styles/general.scss" as *;
+@use "../styles/partials/variables.scss" as *;
+@use "bootstrap/scss/bootstrap.scss" as *;
 </style>
