de3835cb9a90012123bb4183a670c0b327af626b
francescotrudu <francescotrudu84@gmail.com>
2023-01-16 12:29:14 +0100

    Add axios request

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,34 @@ export default {
 
   data() {
     return {
-      store
+      store,
+      apiUrl: 'https://api.themoviedb.org/3/movie',
+      apiKey: '608d618b027efbcefdc264f920beae3a',
+      queryString: 'Harry',
 
     }
+  },
+
+  methods: {
+    getMovies() {
+      axios.get(this.apiUrl, {
+        params: {
+          api_key: this.apiKey,
+          query: this.queryString,
+        }
+      })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+    }
+  },
+
+  created() {
+    this.getMovies();
   },
 }
 </script>
@@ -24,4 +49,5 @@ export default {
 <style lang="scss">
 @use "./styles/general.scss" as *;
 @use "./styles/partials/variables.scss" as *;
+@use "bootstrap/scss/bootstrap.scss" as *;
 </style>
