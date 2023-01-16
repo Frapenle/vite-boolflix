27579acb27b215809d44bb8d827c9bf41b899b4e
francescotrudu <francescotrudu84@gmail.com>
2023-01-16 19:05:07 +0100

    Add content to a card

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -16,32 +16,28 @@ export default {
 </script>
 
 <template>
-  <header>
+  <header class="container-fluid">
     <h1>Header</h1>
     <label class="me-2" for="searchbar">Cerca</label>
     <input type="text" name="" placeholder="Inserire titolo" id="searchbar" v-model="store.searchText"
       @keyup.enter="$emit('search')">
     <button class="btn btn-danger ms-2" @click="$emit('search')">Cerca</button>
+    <h3>Film</h3>
     <ul>
-      <li>
-        <h2>Film</h2>
-      </li>
-      <li v-for="movie in store.movies" :key="movie.id" class="mb-5">
-        <img :src="`${coverPath}${coverDimension}${movie.poster_path}`" :alt="movie.name">
-        <h3>{{ movie.title }}</h3>
-        <h4>{{ movie.original_title }}</h4>
+      <li v-for="movie in store.movies" :key="movie.id" class="mb-5 card">
+        <img :src="`${coverPath}${coverDimension}${movie.poster_path}`" :alt="movie.title" class="card-img-top">
+        <h5>{{ movie.title }}</h5>
+        <h6>{{ movie.original_title }}</h6>
         <p>Lingua {{ movie.original_language }} Voto: {{ movie.vote_average }}</p>
       </li>
     </ul>
 
+    <h3>Serie TV</h3>
     <ul>
-      <li>
-        <h2>Serie TV</h2>
-      </li>
-      <li v-for="serie in store.tvSeries" :key="serie.id">
-        <img :src="`${coverPath}${coverDimension}${serie.poster_path}`" :alt="serie.name">
-        <h3>{{ serie.name }}</h3>
-        <h4>{{ serie.original_name }} </h4>
+      <li v-for="serie in store.tvSeries" :key="serie.id" class="mb-5 card">
+        <img :src="`${coverPath}${coverDimension}${serie.poster_path}`" :alt="serie.name" class="card-img-top">
+        <h5>{{ serie.name }}</h5>
+        <h6>{{ serie.original_name }} </h6>
         <p>Lingua {{ serie.original_language }} Voto: {{ serie.vote_average }}</p>
       </li>
     </ul>
@@ -53,4 +49,14 @@ export default {
 @use "../styles/general.scss" as *;
 @use "../styles/partials/variables.scss" as *;
 @use "bootstrap/scss/bootstrap.scss" as *;
+
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+
+.card {
+  width: calc(100% / 5 - 1rem);
+}
 </style>
