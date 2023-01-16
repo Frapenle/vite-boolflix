b8049a5c96fa86bf0e91242ef6d19ec4f7e25873
francescotrudu <francescotrudu84@gmail.com>
2023-01-16 09:35:22 +0100

    Add axios

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
+import axios from 'axios';
 
 export default {
   components: {
@@ -14,5 +15,5 @@ export default {
 
 <style lang="scss">
 @use "./styles/general.scss" as *;
-@use "./styles/partials/variables.scss" as *;
+@use "./styles/partialns/variables.scss" as *;
 </style>
