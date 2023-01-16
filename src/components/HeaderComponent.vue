9e25654cf7693756c958bb94f1199c04b73c1b2c
francescotrudu <francescotrudu84@gmail.com>
2023-01-16 17:33:10 +0100

    Add tv series list

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -19,13 +19,23 @@ export default {
     <label class="me-2" for="searchbar">Cerca</label>
     <input type="text" name="" placeholder="Inserire titolo" id="searchbar" v-model="store.searchText"
       @keyup.enter="$emit('search')">
-    <button class="btn btn-danger ms-2" @click="$emit('search')">Cerca</button>
+    <button class="btn btn-danger ms-2" @click="$emit('searchTvSerie')">Cerca</button>
     <ul>
+      <li>
+        <h2>Film</h2>
+      </li>
       <li v-for="movie in store.movies" :key="movie.id" class="mb-5">
         <h3>{{ movie.title }}</h3>
         <h4>{{ movie.original_title }}</h4>
         <p>Lingua {{ movie.original_language }} Voto: {{ movie.vote_average }}</p>
       </li>
+    </ul>
+
+    <ul>
+      <li>
+        <h2>Serie TV</h2>
+      </li>
+      <li v-for="serie in store.tvSeries"> {{ serie }}</li>
     </ul>
 
   </header>
