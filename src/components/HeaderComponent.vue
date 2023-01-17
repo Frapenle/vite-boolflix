e0cc19a9a4309fddf0637616b63d739cc69d5f0f
francescotrudu <francescotrudu84@gmail.com>
2023-01-17 10:54:29 +0100

    Add logo header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -17,38 +17,14 @@ export default {
 
 <template>
   <header class="container-fluid">
-    <h1>Header</h1>
-    <label class="me-2" for="searchbar">Cerca</label>
-    <input type="text" name="" placeholder="Inserire titolo" id="searchbar" v-model="store.searchText"
-      @keyup.enter="$emit('search')">
-    <button class="btn btn-danger ms-2" @click="$emit('search')">Cerca</button>
-    <!-- <h3>Film</h3>
-    <ul>
-      <li v-for="movie in store.movies" :key="movie.id" class="mb-5 card">
-        <div class="img-wrapper">
-          <img :src="`${coverPath}${coverDimension}${movie.poster_path}`" :alt="movie.title" class="card-img-top">
-        </div>
-        <div class="card-body">
-          <h5>{{ movie.title }}</h5>
-          <h6>{{ movie.original_title }}</h6>
-          <p>Lingua {{ movie.original_language }} - Voto: {{ movie.vote_average }}</p>
-        </div>
-      </li>
-    </ul>
-
-    <h3>Serie TV</h3>
-    <ul>
-      <li v-for="serie in store.tvSeries" :key="serie.id" class="mb-5 card">
-        <div class="img-wrapper">
-          <img :src="`${coverPath}${coverDimension}${serie.poster_path}`" :alt="serie.name" class="card-img-top">
-        </div>
-        <div class="card-body">
-          <h5>{{ serie.name }}</h5>
-          <h6>{{ serie.original_name }} </h6>
-          <p>Lingua {{ serie.original_language }} - Voto: {{ serie.vote_average }}</p>
-        </div>
-      </li>
-    </ul> -->
+    <div class="logo">
+      <img src="../assets/img/logo.svg" alt="Netflix logo">
+    </div>
+    <div class="search">
+      <input type="text" name="" placeholder="Inserire titolo" id="searchbar" v-model="store.searchText"
+        @keyup.enter="$emit('search')">
+      <button class="btn btn-danger ms-2" @click="$emit('search')">Cerca</button>
+    </div>
 
   </header>
 </template>
@@ -57,4 +33,23 @@ export default {
 @use "../styles/general.scss" as *;
 @use "../styles/partials/variables.scss" as *;
 @use "bootstrap/scss/bootstrap.scss" as *;
+
+header {
+  width: 100%;
+  height: 70px;
+  display: flex;
+  background-color: black;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.logo {
+  width: 100px;
+
+  img {
+    width: 100%;
+  }
+}
+
+.search {}
 </style>
