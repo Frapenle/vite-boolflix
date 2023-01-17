92328ea8efff49a349bdf4cbf70208516768d869
francescotrudu <francescotrudu84@gmail.com>
2023-01-17 17:04:46 +0100

    Add hover overview

--- src/components/MovieComponent.vue
+++ src/components/MovieComponent.vue
@@ -7,17 +7,32 @@
                     <img v-if="movie.poster_path != null" :src="`${coverPath}${coverDimension}${movie.poster_path}`" :alt="movie.title" class="cover-img card-img-top">
                     <img v-else src="http://www.tecno-store.it/wp-content/uploads/immagine-non-disponibile-q.png" :alt="movie.title" class="fakeimg card-img-top">
                     <div class="hover">
-                        <div class="text">John Doe</div>
+                        <div class="title">
+                            <p v-if="movie.title != movie.original_title"><span>Titolo: </span>{{ movie.title }}</p>
+                            <p v-else><span>Titolo: </span> {{ movie.original_title }}</p>
+                        </div>
+                        <div class="overview">
+                            <p><span>Overview: </span>{{ movie.overview }}</p>
+                        </div>
+                        <div class="language"><span>Lingua: </span>
+                            <img v-if="langFlags.includes(movie.original_language)" :src="getImagePath(movie.original_language)">
+                            <span v-else>{{ movie.original_language }}</span>
+                        </div>
+                        <div class="ratings"><span>Voto: </span>
+                            <font-awesome-icon icon="fa-solid fa-star" v-for="star in Math.floor(movie.vote_average / 2)"/>
+                            <font-awesome-icon icon="fa-regular fa-star" v-for="blank_star in Math.ceil(5 - (movie.vote_average / 2))"/>
+                        </div>
+
                     </div>
                 </div>
                 
                 <div class="card-body">
                     <h6 v-if="movie.title != movie.original_title">{{ movie.title }}</h6>
                     <h6 v-else>{{ movie.original_title }}</h6>
-                    <div class="language">Lingua:
+                    <!-- <div class="language">Lingua:
                         <img v-if="langFlags.includes(movie.original_language)" :src="getImagePath(movie.original_language)">
                         <span v-else>{{ movie.original_language }}</span>
-                    </div>
+                    </div> -->
                     <div class="ratings">
                         <font-awesome-icon icon="fa-solid fa-star" v-for="star in Math.floor(movie.vote_average / 2)"/>
                         <font-awesome-icon icon="fa-regular fa-star" v-for="blank_star in Math.ceil(5 - (movie.vote_average / 2))"/>
@@ -76,6 +91,7 @@ export default {
 }
 
 .hover {
+    height: 100%;
     padding: 1rem .5rem;
     padding: 1rem .5rem;
     transition: .5s ease;
@@ -83,11 +99,24 @@ export default {
     position: absolute;
     top: 0;
     left: 0;
+
+    p {
+        font-size: .9rem;
+    }
+
+    span {
+        font-weight: bold;
+    }
+}
+
+.overview {
+    height: 70%;
+    overflow-y: auto;
 }
 
 
 .img-wrapper:hover .cover-img {
-    opacity: 0.3;
+    opacity: 0.2;
 }
 
 .img-wrapper:hover .hover {
