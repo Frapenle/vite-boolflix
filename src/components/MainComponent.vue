3fcbb4b5ed988db4a161904747576242aa21695d
francescotrudu <francescotrudu84@gmail.com>
2023-01-17 13:09:44 +0100

    Add main component

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,30 @@
+<script>
+import MovieComponent from './MovieComponent.vue';
+import TvSeriesComponent from './TvSeriesComponent.vue';
+export default {
+    name: 'MainComponent',
+    components: {
+        MovieComponent,
+        TvSeriesComponent,
+    },
+    data() {
+        return {
+
+        }
+    },
+
+}
+</script>
+
+<template lang="">
+    <main>
+        <MovieComponent />
+        <TvSeriesComponent />
+        
+    </main>
+</template>
+<style lang="scss" scoped>
+main {
+    padding-top: 75px;
+}
+</style>
